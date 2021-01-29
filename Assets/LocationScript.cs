48daeae8e8d181fb615c9d0e8f138843346aca9b
Spencer Shao <shao.sp@northeastern.edu>
2021-01-28 20:34:04 -0500

    MicrophoneTest

--- Assets/LocationScript.cs
+++ Assets/LocationScript.cs
@@ -53,6 +53,8 @@ public class LocationScript : MonoBehaviour
             PrintLocation.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
         }
 
+        
+
         // Stop service if there is no need to query location updates continuously
         Input.location.Stop();
     }
